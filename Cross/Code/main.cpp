eddf9a4f6590556514d81d8bcfeecb51369a780a
ryunosuke <golovanus666@gmail.com>
2018-12-05 04:19:08 +0300

    Update main.cpp
    промежуточная версия с персонажем и платформой

--- Cross/Code/main.cpp
+++ Cross/Code/main.cpp
@@ -1,14 +1,40 @@
-
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
+using namespace sf;
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(600, 800), "Lesson 2. kychka-pc.ru");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Red);
+    float x1, y1, x2, y2;
+    
+    RenderWindow window(sf::VideoMode(1280, 1280), "EASY GAME 228");
+    
+    Texture herotexture;
+    herotexture.loadFromFile("Resources/hero.png");
+    
+    Sprite herosprite;
+    herosprite.setTexture(herotexture);
+    herosprite.setTextureRect(IntRect(0, 192, 96, 96));
+    herosprite.setPosition(592, 1280-96-64);
+    
+    Texture platformtexture;
+    platformtexture.loadFromFile("Resources/platform.jpg");
+    
+    Sprite platformsprite;
+    platformsprite.setTexture(platformtexture);
+    platformsprite.setTextureRect(IntRect(0, 0, 256, 64));
+    platformsprite.setPosition(512, 1280-64);
+    
+    float CurrentFrame = 0;  //хранит текущий кадр
+    Clock clock;
     
     while (window.isOpen())
     {
+        
+        float time = clock.getElapsedTime().asMicroseconds();
+        clock.restart();
+        time = time / 800;
+        
+        
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -16,8 +42,37 @@ int main()
                 window.close();
         }
         
+        
+        ///////////////////////////////////////////Управление персонажем с анимацией////////////////////////////////////////////////////////////////////////
+        
+        
+        
+        if ((Keyboard::isKeyPressed(Keyboard::Left))) { //если нажата клавиша стрелка влево
+            CurrentFrame += 0.005*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
+            if (CurrentFrame > 3) CurrentFrame -= 3; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
+            herosprite.setTextureRect(IntRect(96 * int(CurrentFrame), 96, 96, 96)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
+            herosprite.move(-0.1*time, 0);//происходит само движение персонажа влево
+        }
+        
+        if ((Keyboard::isKeyPressed(Keyboard::Right))) {
+            CurrentFrame += 0.005*time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
+            if (CurrentFrame > 3) CurrentFrame -= 3; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
+            herosprite.setTextureRect(IntRect(96 * int(CurrentFrame), 192, 96, 96)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
+            
+            herosprite.move(0.1*time, 0);//происходит само движение персонажа вправо
+            
+        }
+        
+        // ОБРАБОТКА ВЗАИМОДЕЙСТВИЯ ПЕРСОНАЖА С ПЛАТФОРМОЙ
+        x1 = herosprite.getPosition().x;        // координата х персонажа
+        y1 = herosprite.getPosition().y;        // координата у персонажа 
+        x2 = platformsprite.getPosition().x;    // координата х платформы
+        y2 = platformsprite.getPosition().y;    // координата у платформы
+
+        
         window.clear();
-        window.draw(shape);
+        window.draw(herosprite);
+        window.draw(platformsprite);
         window.display();
     }
     
